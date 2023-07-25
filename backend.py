38f41e9c01de2c3959cfd474cb364dbca6487d3f
theay <theahadcolo@gmail.com>
2023-07-25 15:55:32 -0500

    Day 33: Complete the plots, add images and change units from Kelvin to Metric

--- backend.py
+++ backend.py
@@ -1,17 +1,13 @@
 import requests
 
 API_KEY = "3dd4f4e56e62b99480dcd9ee2ded9ba2"
-def get_data(place, forecast_days=None, kind=None):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&units=metric&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 if __name__ == "__main__":
