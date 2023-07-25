300ae93079dbae1736d6626acca51b5cbb3bb075
theay <theahadcolo@gmail.com>
2023-07-25 16:42:44 -0500

    Day 33: Complete the plots, add images and change units from Kelvin to Metric, removed API key

--- backend.py
+++ backend.py
@@ -1,7 +1,11 @@
+import os
 import requests
 
-API_KEY = "3dd4f4e56e62b99480dcd9ee2ded9ba2"
+API_KEY = os.getenv("OPENWEATHER")
+
 def get_data(place, forecast_days=None):
+    if not API_KEY:
+        raise ValueError("OpenWeatherMap API key is missing. Set the OPENWEATHER environment variable.")
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&units=metric&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
@@ -11,4 +15,4 @@ def get_data(place, forecast_days=None):
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=1, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=1))
